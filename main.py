a4d80302c1123263c6ba892300f2b96a508f2d81
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2022-10-24 11:57:07 +0700

    home-start notwork

--- main.py
+++ main.py
@@ -8,23 +8,25 @@ SCREEN_HEIGHT = 500
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Main Menu")
 menu_state = "start"
+font1 = pygame.font.Font("freesansbold.ttf", 48)
 font2 = pygame.font.Font("freesansbold.ttf", 32)
 TEXT_COL = (0, 0, 0)
-resume_img = pygame.image.load("images/button_resume.png").convert_alpha()
-options_img = pygame.image.load("images/button_options.png").convert_alpha()
-quit_img = pygame.image.load("images/button_quit.png").convert_alpha()
-video_img = pygame.image.load('images/button_video.png').convert_alpha()
-audio_img = pygame.image.load('images/button_audio.png').convert_alpha()
-keys_img = pygame.image.load('images/button_keys.png').convert_alpha()
-back_img = pygame.image.load('images/button_back.png').convert_alpha()
-resume_button = button.Button(304, 125, resume_img, 1)
-options_button = button.Button(297, 250, options_img, 1)
-quit_button = button.Button(336, 375, quit_img, 1)
-video_button = button.Button(226, 75, video_img, 1)
-audio_button = button.Button(225, 200, audio_img, 1)
-keys_button = button.Button(246, 325, keys_img, 1)
-back_button = button.Button(332, 450, back_img, 1)
 success_count = 0
+
+start_img = pygame.image.load("images/start.png").convert_alpha()
+tutorail_img = pygame.image.load("images/tutorail.png").convert_alpha()
+scoreboard_img = pygame.image.load('images/scoreboard.png').convert_alpha()
+quit_img = pygame.image.load("images/quit.png").convert_alpha()
+restart_img = pygame.image.load('images/restart.png').convert_alpha()
+home_img = pygame.image.load('images/home.png').convert_alpha()
+
+start_button = button.Button(295, 175, start_img, 0.33)
+tutorail_button = button.Button(295, 250, tutorail_img, 0.33)
+scoreboard_button = button.Button(295, 325, scoreboard_img, 0.33)
+quit_button = button.Button(295, 400, quit_img, 0.33)
+restart_button = button.Button(295, 250, restart_img, 0.33)
+home_button = button.Button(295, 325, home_img, 0.33)
+
 def load_words_file(filename):
     word_file = open(filename, "r")
     lines_read = word_file.readlines()
@@ -44,23 +46,34 @@ def is_word_on_screen(words_on_screen,word):
         if word == words_on_screen[i]["word"]:
             return i 
     return -1
-def scoreboard():
-    pass
-def tutorial():
-    pass
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 run = True
 while run:
-    screen.fill((255, 255, 255))
     if menu_state == "start":
-        if resume_button.draw(screen):
-            menu_state = 'game'
-        if options_button.draw(screen):
-            menu_state = "options"
-        if quit_button.draw(screen):
-            run = False
+        pygame.init()
+        display_surface = pygame.display.set_mode((800, 500))
+        pygame.display.set_caption("Start menu")
+        startrun = True
+        display_surface.fill((255,255,255))
+        while startrun:
+            if start_button.draw(screen):
+                menu_state = 'game'
+                startrun = False
+            if tutorail_button.draw(screen):
+                menu_state = "tutorail"
+                startrun = False
+            if scoreboard_button.draw(screen):
+                menu_state = 'scoreboard'
+                startrun = False
+            if quit_button.draw(screen):
+                run = False
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    startrun = False
+            pygame.display.update()
     if menu_state == "game":
         lines_read = load_words_file("english_words.txt")# load words from the file
         pygame.init()
@@ -78,9 +91,10 @@ while run:
         rungame = True
         pytime = pygame.time.get_ticks()/1000
         while rungame:
-            countdown_timer = (60+pytime) - (pygame.time.get_ticks() / 1000)
+            countdown_timer = (5+pytime) - (pygame.time.get_ticks() / 1000)
             if float(countdown_timer) <= 0.00:#end game
                 display_surface.fill((255,255,255))
+                menu_state = 'end'
                 ended = True
                 rungame = False
             if not ended:
@@ -94,6 +108,7 @@ while run:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         run = False
+                        rungame = False
                     if event.type == pygame.KEYDOWN:
                         if pygame.key.name(event.key) == "return":
                             index_found = is_word_on_screen(words_on_screen,current_text)
@@ -114,35 +129,31 @@ while run:
                 render_text(display_surface,font,str('%.2f'%(countdown_timer)),(0,0,0),(675,450))
                 render_text(display_surface,font,str('%d'%(success_count)),(0,0,0),(750,27))
                 render_text(display_surface,font,'Score:',(0,0,0),(640,25))
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
             pygame.display.update()
             clock.tick(60)
-        menu_state = 'end'
-    if menu_state == "options":
-        if video_button.draw(screen):
-            print("Video Settings")
-        if audio_button.draw(screen):
-            print("Audio Settings")
-        if keys_button.draw(screen):
-            print("Change Key Bindings")
-        if back_button.draw(screen):
-            menu_state = "start"
+    if menu_state == "tutorail":
+        pass
+    if menu_state == 'scoreboard':
+        pass
     if menu_state == 'end':
         pygame.init()
         display_surface = pygame.display.set_mode((800, 500))
         pygame.display.set_caption("End menu")
-        font1 = pygame.font.Font("freesansbold.ttf", 48)
-        font2 = pygame.font.Font("freesansbold.ttf", 32)
         endrun = True
+        display_surface.fill((255,255,255))
         while endrun:
-            display_surface.fill((255,255,255))
             render_text(display_surface,font1,"Game Over.",(0,0,0),(275,35))
-            render_text(display_surface,font2," Score :  {0} word per minute".format(success_count),(0,0,0),(150,450))
+            render_text(display_surface,font2," Score :  {0} word per minute".format(success_count),(0,0,0),(175,125))
+            if restart_button.draw(screen):
+                menu_state = 'game'
+                endrun = False
+            if home_button.draw(screen):
+                menu_state = "start"
+                endrun = False
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+                    endrun = False
             pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
